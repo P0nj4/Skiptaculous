3c974194f6053486674f676c14bf73a01f308dc1
German Pereyra <german.f.pereyra@gmail.com>
2014-07-23 16:50:10 -0300

    the hero can jump

--- Skiptaculous/PlayScene.swift
+++ Skiptaculous/PlayScene.swift
@@ -16,6 +16,9 @@ class PlayScene : SKScene {
     var xMaxBar = CGFloat(0)
     var groundSpeed = CGFloat(2)
     var heroBaseLine = CGFloat(0)
+    var velocityY = CGFloat(0)
+    var onTheGround = true
+    var gravity = CGFloat(0.6)
     
     override func didMoveToView(view: SKView!) {
         println("play scene displayed")
@@ -28,21 +31,49 @@ class PlayScene : SKScene {
         
         self.xMaxBar = self.runningBar.size.width - CGRectGetMaxX(self.frame)
         self.xMaxBar *= -1
-        self.heroBaseLine = self.runningBar.position.y + self.runningBar.frame.height;
+
         self.hero.position = CGPointMake(CGRectGetMinX(self.frame) + self.hero.size.width / 2, self.runningBar.position.y + self.runningBar.size.height + (self.hero.size.height / 2))
-        
+
+        self.heroBaseLine = self.hero.position.y
         self.addChild(hero)
     }
     
     override func update(currentTime: NSTimeInterval) {
+        // ground move
         if(self.runningBar.position.x <= xMaxBar){
             self.runningBar.position.x = self.originBarPosition
         }
+        self.runningBar.position.x -= self.groundSpeed;
         
+        
+        // hero rotation
         var degreeRotation = (self.groundSpeed * 3) * 3.14 / 180
         self.hero.zRotation -= CGFloat(degreeRotation)
         
-        self.runningBar.position.x -= self.groundSpeed;
+        
+
+        // Jump
+        self.velocityY += self.gravity
+        self.hero.position.y -= self.velocityY;
+        if self.hero.position.y < self.heroBaseLine {
+            self.hero.position.y = self.heroBaseLine
+            velocityY = 0.0
+            self.onTheGround = true
+        }
     
+    }
+    
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        if onTheGround {
+            self.velocityY = -18
+            self.onTheGround = false
+            
+        }
+    }
+    
+    override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
+        if self.velocityY < -9.0 {
+            self.velocityY = -9.0
+        }
     }
 }
