ead110a627ea6a67a316fb3d6cba0364a5ec6d61
German Pereyra <german.f.pereyra@gmail.com>
2014-07-23 15:38:27 -0300

    ground is being moved

--- Skiptaculous/PlayScene.swift
+++ Skiptaculous/PlayScene.swift
@@ -10,9 +10,30 @@ import SpriteKit
 
 class PlayScene : SKScene {
     
+    let runningBar = SKSpriteNode(imageNamed: "bar")
+    var originBarPosition = CGFloat(0)
+    var xMaxBar = CGFloat(0)
+    var groundSpeed = CGFloat(2)
+    
     override func didMoveToView(view: SKView!) {
         println("play scene displayed")
         self.backgroundColor = UIColor(hex: 0x80D9FF)
+        
+        self.runningBar.anchorPoint = CGPointMake(0, 0);
+
+        self.runningBar.position = CGPointMake(CGRectGetMinX(self.frame), CGRectGetMinY(self.view.bounds))
+        self.addChild(self.runningBar);
+        
+        self.xMaxBar = self.runningBar.size.width - CGRectGetMaxX(self.frame)
+        self.xMaxBar *= -1
     }
     
+    override func update(currentTime: NSTimeInterval) {
+        if(self.runningBar.position.x <= xMaxBar){
+            self.runningBar.position.x = self.originBarPosition
+        }
+        
+        self.runningBar.position.x -= self.groundSpeed;
+    
+    }
 }
